4c06bf463a5ebabbbeeca117ea3609895f72ae4a
Juan Andrés Nieto de la Vega <jnietodelavega@hotmail.com>
2021-02-15 14:21:07 -0300

    FIXED:  se agrega endpoint (getDemograph) para obtener la demografia. En caso de que un paciente existente no la tenga, se crea. Se cambia el tipo de dato del telefono a string ya que se caía por que algunos venian con caracteres especiales. En el caso que ocurra un error en el WS despues de creada la sospecha en esmeralda se informa el error en el campo "ws_minsal_message".

--- WebService/Startup.cs
+++ WebService/Startup.cs
@@ -137,12 +137,12 @@ namespace WebService
                     c.RoutePrefix = "apolohra/apidocs";
                 }
             );
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-            //app.UseAuthentication();
+            app.UseAuthentication();
             app.UseRouting();
             app.UseCors("CorsPolicy");
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseHealthChecks("/self", new HealthCheckOptions { Predicate = r => r.Name.Contains("self") });
             app.UseHealthChecks("/ready", new HealthCheckOptions { Predicate = r => r.Tags.Contains("service") });
